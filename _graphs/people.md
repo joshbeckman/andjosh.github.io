a6c14e1a88c4d8f1a47447e0e1ce660cbdc32dd7
andjosh <email@andjosh.com>
2015-08-05 19:28:42 -0500

    breaking out graphs

---
+++ _graphs/people.md
@@ -0,0 +1,7 @@
+--
+layout: graph
+title: People I Lived With
+permalink: /graphs/people/
+---
+
+<script src="/js/graphs/people.js"></script>
---
+++ js/graphs/people.js
@@ -0,0 +1,27 @@
+var width = document.body.clientWidth - 60,
+    height = width - 200,
+    svg = dimple.newSvg('#chartContainer', width, height);
+
+d3.csv('/data/livedPeople.csv', function(data) {
+    var myChart = new dimple.chart(svg, data),
+        x,
+        s;
+
+    myChart.setBounds(60, 30, width - 100, height - 100);
+    x = myChart.addCategoryAxis('x', 'Year');
+    x.addOrderRule('Date');
+    myChart.addMeasureAxis('y', 'People');
+    s = myChart.addSeries(
+        'Place',
+        dimple.plot.line
+    );
+    s.interpolation = 'step';
+    myChart.addLegend(
+        60,
+        10,
+        width - 100,
+        20,
+        'right'
+    );
+    myChart.draw();
+});

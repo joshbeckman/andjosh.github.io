45b42148b30b5b9e9e3e20f7bda8d5d1099770ae
andjosh <email@andjosh.com>
2016-10-03 20:28:52 -0500

    Add constellation animation to header
    with gravity effect

--- js/screen.js
+++ js/screen.js
@@ -42,8 +42,10 @@ function color2(num, ti, off) {
     header.style.backgroundImage =
         'linear-gradient(90deg, ' + color2(25, pubTime, 8) +
         ', ' + color2(0, pubTime, 8) + ')';
+    header.setAttribute('data-one', color2(25, pubTime, 8));
+    header.setAttribute('data-two', color2(0, pubTime, 8));
 })(this, this.document);
-(function(window, document) {
+function bouncyBall(window, document) {
     var canvas = document.createElement('canvas'),
         context = canvas.getContext('2d'),
         header = document.querySelector('.header'),
@@ -82,7 +84,7 @@ function color2(num, ti, off) {
         xc = Math.round(Math.random() * 50),
         yc = Math.round(Math.random() * 50);
 
-    var interval = null; //setInterval(tick, 1000/33);
+    var interval = setInterval(tick, 1000/33);
     canvas.addEventListener('click', stop);
  
     function tick() {
@@ -127,4 +129,92 @@ function color2(num, ti, off) {
         interval = null;
     }
     window.canvas = canvas;
-})(this, this.document);
+}
+//bouncyBall(this, this.document);
+function constellation(window, document) {
+    var canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d'),
+        header = document.querySelector('.header'),
+        title = header.querySelector('h1'),
+        w = header.clientWidth,
+        h = header.clientHeight,
+        color = header.style.backgroundColor,
+        ix = w * 2,
+        iy = 0,
+        vy = 0,
+        vx = 0,
+        gr = 1,
+        drawn = [],
+        data = [];
+
+    context.scale(2, 2);
+    document.body.appendChild(canvas);
+    canvas.style.position = 'absolute';
+    canvas.style.cursor = 'pointer';
+    canvas.style.top =
+        canvas.style.left = 0;
+    canvas.setAttribute('width', w * 2);
+    canvas.setAttribute('height', h * 2);
+    canvas.style.width = w + 'px';
+    canvas.style.height = h + 'px';
+    context.strokeStyle = context.fillStyle = 'rgba(255,255,255,0.25)';
+    var lg = context.createLinearGradient(0, 0, w * 2, 0);
+    lg.addColorStop(0, header.dataset.one);
+    lg.addColorStop(1, header.dataset.two);
+    context.fillStyle = lg;
+    context.lineWidth = 2;
+
+    for (var i = w; i >= 0; i--) {
+        data.push([
+            Math.round(Math.random() * w * 2),
+            Math.round(Math.random() * h * 2)
+        ]);
+    }
+    var interval = setInterval(tick, 1000/33),
+        dropped = false;
+    canvas.addEventListener('click', stop);
+ 
+    function tick() {
+        i++;
+        if (i > w) {
+            i = 0;
+        }
+        context.beginPath();
+        context.arc(data[i][0], data[i][1], 1, 0, 2 * Math.PI);
+        context.stroke();
+        drawn.push([data[i][0], data[i][1], vx, vy]);
+    }
+    function drop() {
+        context.fillRect(0, 0, w * 2, h * 2);
+        for (var i = drawn.length - 1; i >= 0; i--) {
+            drawn[i][0] += drawn[i][2];
+            drawn[i][1] += drawn[i][3];
+            if (drawn[i][1] > (h * 2)) {
+                drawn[i][2] *=  0.75;
+                drawn[i][3] *= -0.6;
+            }
+            drawn[i][3] += gr;
+            context.beginPath();
+            context.arc(drawn[i][0], drawn[i][1], 1, 0, 2 * Math.PI);
+            context.stroke();
+        }
+    }
+    function minus(v, n) {
+        return v -= n;
+    }
+    function plus(v, n) {
+        return v += n;
+    }
+    function stop() {
+        clearInterval(interval);
+        if (dropped) {
+            interval = setInterval(tick, 1000/33);
+            dropped = false;
+            return;
+        }
+        interval = setInterval(drop, 1000/33);
+        dropped = true;
+    }
+    window.canvas = canvas;
+}
+constellation(this, this.document);
